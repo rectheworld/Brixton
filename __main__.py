d6c9498e0df4f70b98160503cf9a38b33282505d
Ren <lcdebaca@DC3CDEBACALLT.air.org>
2015-10-05 12:00:51 -0400

    Player can move around screen, animation is not clean

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import *
 
 
 FPS = 60
@@ -17,11 +18,64 @@ class Game(object):
 		self.clock 			= pygame.time.Clock()
 		self.running		= True
 
+		# Used in Process Events
+		self.old_keys		= {} 
+
 		from player import Player
-		self.player			= Player((48,48))
+		self.player			= Player([48,48])
+
+	# Used to create easy movment , called in process events 
+	def difference(self, tuple_1, tuple_2):
+		results = []
+		for i in range(len(tuple_1)): results.append(tuple_1[i] and not tuple_2[i])
+		return tuple(results)
+
 
 	#Think ( A Little bit a Listen)
 	def process_events(self):
+		#Collect the Keystrocks 
+		walk = False 
+		old_pos = (self.player.position[0], self.player.position[1])
+
+		keys = pygame.key.get_pressed()
+
+		# Not toally sure what this code does 
+		if self.old_keys:
+			self.difference(keys, self.old_keys)
+			self.difference(self.old_keys, keys)
+		self.old_keys = keys 
+
+		if keys[K_LEFT]:
+			if self.player.position[0] > 0:
+				self.player.position[0] -= 1 
+			self.player.direction = self.player.WEST
+			self.player.animation = self.player.WALK 
+			walk = True 
+
+		if keys[K_RIGHT]:
+			self.player.position[0] += 1
+			self.player.direction = self.player.EAST
+			self.player.animation = self.player.WALK
+			walk = True
+
+		if keys[K_UP]:
+			if self.player.position[1] > 0:  
+				self.player.position[1] -= 1
+			self.player.direction = self.player.NORTH
+			self.player.animation = self.player.WALK
+			walk = True
+
+		if keys[K_DOWN]:
+			self.player.position[1] += 1 
+			self.player.direction = self.player.SOUTH
+			self.player.animation = self.player.WALK
+			walk = True
+
+		if not walk:
+			self.player.animation = self.player.STAND
+
+		#Update Player Variables 
+		self.player.update() 
 
 
 
@@ -48,7 +102,7 @@ class Game(object):
 			self.screen.fill((255,255,255))
 
 			# this should go into a Process events function 
-			self.player.update()
+			self.process_events()
 
 
 			self.render()
